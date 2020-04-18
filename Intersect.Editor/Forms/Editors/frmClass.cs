4444e6a1f71344be7b4ba7854525669515de1e0d
Joyce <azurebeats@hotmail.nl>
2020-04-18 13:03:12 +0200

    Fixes two editor crashes.
    Signed-off-by: David Ramirez <r.davidsaid06@gmail.com>

--- Intersect.Editor/Forms/Editors/frmClass.cs
+++ Intersect.Editor/Forms/Editors/frmClass.cs
@@ -1504,7 +1504,7 @@ namespace Intersect.Editor.Forms.Editors
             mEditorItem.CustomSpriteLayers[CustomSpriteLayers.Hair].RemoveAt(lstHair.SelectedIndex);
             lstHair.Items.RemoveAt(lstHair.SelectedIndex);
 
-            RefreshSpriteList(false);
+            RefreshHairList(false);
 
             if (lstHair.Items.Count > 0) {
                 lstHair.SelectedIndex = 0;
